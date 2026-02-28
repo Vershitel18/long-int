83828716ae700ad7d959d8e254dba8ff7a93e6b3
Vershitel18 <Ognezvezdg@gmail.com>
2026-02-28 14:34:58 +0300

    sub/add/mul_first_version

--- lib/math/add.asm
+++ lib/math/add.asm
@@ -1,10 +1,6 @@
 section         .text
-
                 cpu             x64
-
                 global          long_int_add
-
-
 ; adds two long numbers
 ;    rdi -- address of summand #1 (long number)
 ;    rsi -- address of summand #2 (long number)
@@ -12,4 +8,12 @@ section         .text
 ; result:
 ;    sum is written to rdi
 long_int_add:
+                clc
+.loop:
+                mov             r9, [rsi]
+                adc             [rdi], r9
+                lea             rdi, [rdi + 8]
+                lea             rsi, [rsi + 8]
+                dec             rdx
+                jnz             .loop
                 ret
